302d6c06465d7dbafe91a1ab7e2cbec2beee9716
RafaelAugustScherer <rafaelaugustscherer@gmail.com>
2022-06-01 19:08:36 -0300

    [back-end]: Add cors

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 require('express-async-errors');
 const appRouter = require('./routers');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(appRouter);
 
 /* app.get('/coffee', (_req, res) => res.status(418).end()); */
