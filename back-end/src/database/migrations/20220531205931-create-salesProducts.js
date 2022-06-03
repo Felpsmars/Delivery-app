b9b5e1e011d2233e31cc181f425ec0c995999bde
rafaelaugustscherer <rafaelaugustscherer@gmail.com>
2022-06-03 17:18:32 -0300

    [back-end] Refactor: Uppercase -> Lowercase table names

--- back-end/src/database/migrations/20220531205931-create-salesProducts.js
+++ back-end/src/database/migrations/20220531205931-create-salesProducts.js
@@ -13,7 +13,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: 'Sales',
+          model: 'sales',
           key: 'id',
         },
       },
@@ -21,7 +21,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: 'Products',
+          model: 'products',
           key: 'id',
         },
       },
--- back-end/src/database/models/Sale.js
+++ back-end/src/database/models/Sale.js
@@ -28,7 +28,7 @@ module.exports = (sequelize, DataTypes) => {
     },
   }, {
     timestamps: false,
-    tableName: 'Sales',
+    tableName: 'sales',
     underscored: true,
   });
 
