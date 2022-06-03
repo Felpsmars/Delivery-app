f8c8ec0de18912356c33b594abf8b39d259f94df
Ricardo Fonseca Zabir <ricardofonseca.zabir@hotmail.com>
2022-06-03 18:49:01 -0300

    fix data-testid

--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -2,25 +2,25 @@ import React from 'react';
 
 const Navbar = () => (
   <ul className="navbar">
-    <li data-testid="customer_products__element-navbar-link-products">
-
-      Produtos
-
+    <li>
+      <a data-testid="customer_products__element-navbar-link-products">
+        Produtos
+      </a>
     </li>
-    <li data-testid="customer_products__element-navbar-link-orders">
-
-      Meus Pedidos
-
+    <li>
+      <a data-testid="customer_products__element-navbar-link-orders">
+        Meus Pedidos
+      </a>
     </li>
-    <li data-testid="customer_products__element-navbar-user-full-name">
-
-      Nome
-
+    <li>
+      <a data-testid="customer_products__element-navbar-user-full-name">
+        Nome
+      </a>
     </li>
-    <li data-test-id="customer_products__element-navbar-link-logout">
-
-      Sair
-
+    <li>
+      <a data-test-id="customer_products__element-navbar-link-logout">
+        Sair
+      </a>
     </li>
   </ul>
 );
