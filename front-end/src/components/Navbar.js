f6d178239655d0397eaaf2af4199da01a9c67a81
rafaelaugustscherer <rafaelaugustscherer@gmail.com>
2022-06-06 14:49:08 -0300

    [front-end] Add: Loading to Provider

--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -1,44 +1,53 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { UserContext } from '../provider/UserProvider';
 
 const Navbar = () => {
+  const [ isLoading, setIsLoading ] = useState(true);
   const { user, logout } = useContext(UserContext);
 
+  useEffect(() => {
+    if (user) setIsLoading(false);
+  }, [user]);
+
   return (
-    <ul className="navbar">
-      <li>
-        <a
-          data-testid="customer_products__element-navbar-link-products"
-          href="/customer/products"
-        >
-          Produtos
-        </a>
-      </li>
-      <li>
-        <a
-          data-testid="customer_products__element-navbar-link-orders"
-          href="/customer/products"
-        >
-          Meus Pedidos
-        </a>
-      </li>
-      <li>
-        <span
-          data-testid="customer_products__element-navbar-user-full-name"
-        >
-          { user.name }
-        </span>
-      </li>
-      <li>
-        <a
-          data-testid="customer_products__element-navbar-link-logout"
-          href="/"
-          onClick={ () => logout() }
-        >
-          Sair
-        </a>
-      </li>
-    </ul>
+    isLoading ? (
+      <p>Carregando Navbar...</p>
+    ) : (
+      <ul className="navbar">
+        <li>
+          <a
+            data-testid="customer_products__element-navbar-link-products"
+            href="/customer/products"
+          >
+            Produtos
+          </a>
+        </li>
+        <li>
+          <a
+            data-testid="customer_products__element-navbar-link-orders"
+            href="/customer/products"
+          >
+            Meus Pedidos
+          </a>
+        </li>
+        <li>
+          <span
+            data-testid="customer_products__element-navbar-user-full-name"
+          >
+            {user.name}
+          </span>
+        </li>
+        <li>
+          <a
+            data-testid="customer_products__element-navbar-link-logout"
+            href="/"
+            onClick={() => logout()}
+          >
+            Sair
+          </a>
+        </li>
+      </ul>
+    )
   );
 };
 
