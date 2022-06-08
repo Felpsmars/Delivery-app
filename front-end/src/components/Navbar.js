d0563c485a3f57f28de67e927a256deff5b14d02
rafaelaugustscherer <rafaelaugustscherer@gmail.com>
2022-06-08 11:47:55 -0300

    [front-end] Update: Fix test run

--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 import { UserContext } from '../provider/UserProvider';
 
 const Navbar = ({ pageName }) => {
--- front-end/src/provider/UserProvider.js
+++ front-end/src/provider/UserProvider.js
@@ -5,8 +5,8 @@ import axios from 'axios';
 export const UserContext = createContext();
 
 const UserProvider = ({ children }) => {
-  const [user, setUser] = useState({});
-  const [isUserValid, setIsUserValid] = useState(undefined);
+  const [ user, setUser ] = useState({});
+  const [ isUserValid, setIsUserValid ] = useState(undefined);
 
   const fetchUser = async () => {
     const currentUser = localStorage.getItem('user');
@@ -19,20 +19,22 @@ const UserProvider = ({ children }) => {
 
   const validateUser = async () => {
     const { REACT_APP_SERVER } = process.env;
-    const UNAUTHORIZED = 401;
 
     const localStorageUser = localStorage.getItem('user');
 
     setIsUserValid(undefined);
-    if (user.token) {
-      const validation = await axios.get(`${REACT_APP_SERVER}/validateToken`, {
-        headers: {
-          authorization: user.token,
-        },
-      });
-      if (validation.status === UNAUTHORIZED) setIsUserValid(false);
-      else setIsUserValid(true);
-    } else if (!localStorageUser) {
+    try {
+      if (user.token) {
+        await axios.get(`${REACT_APP_SERVER}/validateToken`, {
+          headers: {
+            authorization: user.token,
+          },
+        });
+        setIsUserValid(true);
+      } else if (!localStorageUser) {
+        setIsUserValid(false);
+      }
+    } catch (e) {
       setIsUserValid(false);
     }
   };
@@ -58,7 +60,7 @@ const UserProvider = ({ children }) => {
 
   useEffect(() => {
     validateUser();
-  }, [user]);
+  }, [ user ]);
 
   const value = {
     user,
@@ -68,8 +70,8 @@ const UserProvider = ({ children }) => {
   };
 
   return (
-    <UserContext.Provider value={ value }>
-      { children }
+    <UserContext.Provider value={value}>
+      {children}
     </UserContext.Provider>
   );
 };
