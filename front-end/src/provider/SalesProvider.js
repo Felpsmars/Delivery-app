d01aed36e1bb3eaa5c9a8c1f82cae9456a32b1e8
rafaelaugustscherer <rafaelaugustscherer@gmail.com>
2022-06-08 16:23:58 -0300

    [front-end] Add: proptypes import em SalesProvider

--- front-end/src/provider/SalesProvider.js
+++ front-end/src/provider/SalesProvider.js
@@ -1,4 +1,5 @@
 import React, { createContext, useContext, useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 import axios from 'axios';
 import { CartContext } from './CartProvider';
 import { UserContext } from './UserProvider';
