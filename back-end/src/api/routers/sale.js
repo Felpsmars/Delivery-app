f9c184e5d638b0a327a101f15e81861444c2dff8
rafaelaugustscherer <rafaelaugustscherer@gmail.com>
2022-06-08 14:48:11 -0300

    [back-end] Add: Autenticação para rotas de vendas

--- back-end/src/api/routers/sale.js
+++ back-end/src/api/routers/sale.js
@@ -2,22 +2,26 @@ const { Router } = require('express');
 
 const saleController = require('../controllers/sale');
 const saleMiddleware = require('../middlewares/sale');
+const authMiddleware = require('../middlewares/auth');
 
 const saleRouter = Router();
 
 saleRouter.route('/sale')
     .post(
+        authMiddleware.validateToken,
         saleMiddleware.validateCreate,
         saleController.create,
     );
 
 saleRouter.route('/sale/:userId')
     .get(
+        authMiddleware.validateToken,
         saleController.getAll,
     );
 
 saleRouter.route('/delivered/:id')
     .patch(
+        authMiddleware.validateToken,
         saleController.saleDelivered,
     );
 
