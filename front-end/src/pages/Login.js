3afc558db9a93b3cc021e63a12ed8257dc4fc821
rafaelaugustscherer <rafaelaugustscherer@gmail.com>
2022-06-15 18:51:17 -0300

    [front-end] Add: Login CSS

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -2,6 +2,8 @@ import React, { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { UserContext } from '../provider/UserProvider';
+import appIcon from '../assets/app-icon.jpg';
+import style from './style/Login.module.css';
 
 const Login = () => {
   const { user, updateUser, isUserValid } = useContext(UserContext);
@@ -50,59 +52,65 @@ const Login = () => {
   }, [isLogged, navigate, isUserValid, user]);
 
   return (
-    <form className="form-login">
-      <label className="label" htmlFor="email">
-        Email:
-        <input
-          data-testid="common_login__input-email"
-          type="email"
-          name="email"
-          value={ email }
-          className="email"
-          placeholder="Digite seu email"
-          onChange={ ({ target: { value } }) => setEmail(value) }
-          required
-        />
-      </label>
+    <div className={ style.loginContainer }>
+      <img src={ appIcon } alt="app logo" className={ style.appIcon } />
+      <h1>Delivery App</h1>
+      <form className={ style.loginForm }>
+        <label className={ style.loginLabel } htmlFor="email">
+          Login
+          <input
+            data-testid="common_login__input-email"
+            type="email"
+            name="email"
+            value={ email }
+            className="email"
+            placeholder="email@trybeer.com.br"
+            onChange={ ({ target: { value } }) => setEmail(value) }
+            required
+          />
+        </label>
 
-      <label className="label" htmlFor="password">
-        Senha:
-        <input
-          type="password"
-          name="senha"
-          data-testid="common_login__input-password"
-          value={ password }
-          className="senha"
-          placeholder="Digite sua senha"
-          onChange={ ({ target: { value } }) => setPassword(value) }
-          required
-        />
-      </label>
-
-      <button
-        type="button"
-        onClick={ login }
-        className="button"
-        data-testid="common_login__button-login"
-        disabled={ !areFieldsValid }
-      >
-        Entrar
-      </button>
-      <button
-        type="button"
-        className="button"
-        data-testid="common_login__button-register"
-        onClick={ () => navigate('/register') }
-      >
-        Register
-      </button>
+        <label className={ style.loginLabel } htmlFor="password">
+          Senha
+          <input
+            type="password"
+            name="senha"
+            data-testid="common_login__input-password"
+            value={ password }
+            className="senha"
+            placeholder="***********"
+            onChange={ ({ target: { value } }) => setPassword(value) }
+            required
+          />
+        </label>
+        <div className={ style.formButtonContainer }>
+          <button
+            type="button"
+            onClick={ login }
+            className={ style.formLoginButton }
+            data-testid="common_login__button-login"
+            disabled={ !areFieldsValid }
+          >
+            LOGIN
+          </button>
+          <button
+            type="button"
+            className={ style.formRegisterButton }
+            data-testid="common_login__button-register"
+            onClick={ () => navigate('/register') }
+          >
+            Ainda não tenho conta
+          </button>
+        </div>
+      </form>
       {wrongLogin && (
         <p
           data-testid="common_login__element-invalid-email"
+          className={ style.loginError }
         >
-          MESSAGE
+          Usuário ou senha inválidos!
         </p>)}
-    </form>
+    </div>
   );
 };
 
