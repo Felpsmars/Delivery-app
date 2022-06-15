da7b5091a703ce4c3acb8337b2f21469d55d4b2b
rafaelaugustscherer <rafaelaugustscherer@gmail.com>
2022-06-15 15:40:54 -0300

    [front-end] Add: CSS da navbar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Route, Navigate, Routes } from 'react-router-dom';
-import './App.css';
 import Login from './pages/Login';
 import Products from './pages/Products';
 import CreateUser from './pages/CreateUser';
